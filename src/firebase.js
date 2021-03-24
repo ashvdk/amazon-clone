62f8d58074405947eb3b1e60b11b902b9a15a51f
ashwin rao <ashwinvdrk@gmail.com>
2021-03-24 21:41:29 +0530

    react project

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA8jhkRsqPKn5T68a3jPN0_sZ4QOUynqjU",
+  authDomain: "clone-99548.firebaseapp.com",
+  projectId: "clone-99548",
+  storageBucket: "clone-99548.appspot.com",
+  messagingSenderId: "212696740239",
+  appId: "1:212696740239:web:58a8e9dd393a0803a05bfa",
+  measurementId: "G-4TDK62N1LS"
+};
